92a046195229d3ef7ffb22c08062ec33baa00789
Gao Bill <billsfriend1999@outlook.com>
2022-12-27 18:54:40 +0800

    add 221225 HEL ELISA result. use pivot_wider

--- ELISA/elisa.R
+++ ELISA/elisa.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggpubr)
 
+# data 221105 --------------
 read_delim('ELISA/data/elisa-fcgr2b-ova-z93-d14.txt') -> data
 
 data %>%
@@ -16,6 +17,22 @@ data %>%
   facet_wrap(~subtype)+
   scale_y_continuous(expand = expansion(mult = c(0,0.1)))
   
-  
+# data 221226 -----------
+read_delim('ELISA/data/elisa-fcgr2b-hel-aln-d7-17.txt') -> data
 
+data %>%
+  pivot_longer(3:4, names_to = 'group', values_to = 'titer') %>%
+  filter(!is.na(titer)) %>%
+  mutate(day = str_glue('d{day}')) %>%
+  filter(day == 'd17') %>%
+  ggplot(aes(group, titer, color = group)) +
+  stat_summary(geom = 'col', fun = 'mean', fill = 'white', linewidth = 1)+
+  stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.3)+
+  geom_jitter(width = 0.1, height = 0, size = 2)+
+  expand_limits(y = 0)+
+  ggprism::theme_prism()+
+  stat_compare_means(comparisons = list(c('II','IT')), method = 't.test')+
+  scale_color_discrete(label = c('II (n=6)','IT (n=5)'))+
+  facet_wrap(~type+day, scales = 'free')+
+  scale_y_continuous(expand = expansion(mult = c(0,0.1)))
 
