84de7af8511e9f3d3ef186d3cc5625b502b781d5
Gao Bill <billsfriend1999@outlook.com>
2022-12-27 20:40:01 +0800

    tried using snakemake to workflow salmon quant in local PC.

---
+++ IgG-IgM-B/ref/salmon.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for fn in ~/Documents/DataAnalysis/IgG-IgM-B/data/B{1..3};
+do
+samp=`basename ${fn}`
+echo "Processing sample ${samp}"
+salmon quant -i salmon_mouse_index -l A \
+         -1 ${fn}/${samp}_1.clean.fq.gz \
+         -2 ${fn}/${samp}_2.clean.fq.gz \
+         -p 8 --validateMappings --gcBias -o quants/${samp}_quant
+done 
